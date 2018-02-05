f7613815499f98ffd1b1912c577e10d72c266207
Jordan Reed <jordanr23@gmail.com>
2018-02-05 15:10:44 -0700

    attempt heroku deploy 3

--- models/index.js
+++ models/index.js
@@ -8,27 +8,32 @@ require('dotenv').config();
 // const config = require(__dirname + './../config/config.js')[env];
 let sequelize = '';
 
+
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres',
-    logging: false,
+    // protocol: 'postgres',
+    // logging: false,
     ssl: true,
     dialectOptions: {
       ssl: true,
     },
   });
-  console.log(process.env.DATABASE_URL);
+  console.log("using heroku db");
 } else {
   sequelize = new Sequelize('entendre', 'jordan', 'postgres', {
     dialect: 'postgres',
     operatorsAliases: false,
   });
+  console.log("using local db");
 }
 
-
-// var env = process.env.NODE_ENV || 'development';
-// var config = require(__dirname + '/../config/config.js')[env];
 const models = {
   User: sequelize.import('./users'),
   Artist: sequelize.import('./artists'),
@@ -37,11 +42,6 @@ const models = {
   Tag: sequelize.import('./tags'),
   Device: sequelize.import('./devices'),
 };
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 Object.keys(models).forEach((modelName) => {
   if ('associate' in models[modelName]) {
